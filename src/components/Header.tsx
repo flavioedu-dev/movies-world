29ea5a904c6afa430d7ad8f69ca6dd4930235a3a
Flávio Eduardo <xflax00@gmail.com>
2023-06-20 23:43:54 -0300

    feat: added navigate between routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,33 @@
+// Hooks
+import  { useNavigate } from "react-router-dom"
+
 // Components
 import { Navbar } from "./Navbar.style"
 
 interface menuOption{
   name: string;
-  onclick?: () => void;
+  handleClick?: () => void;
 }
 
 const Header = () => {
 
+  const navigate = useNavigate()
+
   const menuOptions: menuOption[] = [
     {
-      name: "Home"
+      name: "Home",
+      handleClick: () => navigate("/")
     },
     {
-      name: "Top Rated"
+      name: "Top Rated",
+      handleClick: () => navigate("/top-rated")
     }
   ]
   return (
     <header>
       <Navbar>
         {menuOptions.map((menu, key) => (
-          <span key={key}>{menu.name}</span>
+          <span key={key} onClick={menu.handleClick}>{menu.name}</span>
         ))}
       </Navbar>
     </header>
