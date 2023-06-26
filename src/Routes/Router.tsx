2424813a2794e28caa75f4614f0ba50efc2fb576
Flávio Eduardo <xflax00@gmail.com>
2023-06-26 06:56:17 -0300

    fix: repairing routes file

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,20 +1,12 @@
 import App from "../App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Pages
-import PopularMovies from "../pages/PopularMovies.tsx";
-import TopRatedMovies from "../pages/TopRatedMovies.tsx";
-import UpComingMovies from "../pages/UpComingMovies.tsx";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<App />}>
-          <Route path="/" element={<PopularMovies />} />
-          <Route path="/top-rated" element={<TopRatedMovies />} />
-          <Route path="/upcoming" element={<UpComingMovies />} />
-        </Route>
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   );
