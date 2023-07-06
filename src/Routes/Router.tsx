01bead600968dfacf3fb63af12189e68ade1ee39
Flávio Eduardo <xflax00@gmail.com>
2023-07-05 23:10:59 -0300

    feat: add movie detail route and initialized component

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,6 +1,7 @@
 import App from "../App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home.tsx";
+import MovieDetails from "../pages/MovieDetails.tsx";
 
 
 const Router = () => {
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route element={<App />} >
           <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
