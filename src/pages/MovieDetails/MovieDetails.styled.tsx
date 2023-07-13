888f083602a0f6d1b30a528fa56aca08ebe85d59
Flávio Eduardo <xflax00@gmail.com>
2023-07-12 21:12:17 -0300

    fix: favorite button styles

--- src/pages/MovieDetails/MovieDetails.styled.tsx
+++ src/pages/MovieDetails/MovieDetails.styled.tsx
@@ -6,7 +6,8 @@ img {
   max-width: 100%;
 }
 
-& > div {
+.details-container {
+  position: relative;
   padding: 0 1.5rem;
 }
 
@@ -90,21 +91,24 @@ img {
   font-size: 1rem;
 }
 
-.favorite_movie {
-  height: 4rem;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
+.favorite_movie { 
   position: fixed;
-  bottom: .5rem;
-  width: 100%;
-}
+  bottom: 1.2rem;
+  right: .7rem;
 
-.favorite_movie p {
   background: linear-gradient(to right, rgb(211, 176, 51) 10%, rgb(224, 196, 94) 90%);
   color: #000;
+  font-size: 1rem;
   font-weight: 500;
+
   padding: .5rem 1rem;
   border-radius: 3rem;
+  border: none;
+}
+
+@media (min-width: 501px) {
+
+  max-width: 70%;
+  margin: 0 auto;
 }
 `
--- src/pages/MovieDetails/MovieDetails.tsx
+++ src/pages/MovieDetails/MovieDetails.tsx
@@ -6,7 +6,6 @@ const MovieDetails = () => {
   const { id } = useParams()
 
   const details = useGetMovieDetails(`https://api.themoviedb.org/3/movie/${id?.replace(":", "")}?language=pt-BR`)
-  console.log(details)
 
   return (
     <StyledMovieDetails>
@@ -14,30 +13,32 @@ const MovieDetails = () => {
 
       <img src={`https://image.tmdb.org/t/p/w500/${details?.poster_path}`} alt="cover" />
 
-      <div className="feedback">  
-        <span>IMDB {details?.vote_average.toFixed(1)}</span>
-        <div>
-          <img src="https://img.icons8.com/?size=512&id=8uAtuJQJ4jhd&format=png" alt="" />
-          <span>{details?.vote_count}</span>
-          
+      <section className="details-container">      
+
+        <div className="feedback">  
+          <span>IMDB {details?.vote_average.toFixed(1)}</span>
+          <div>
+            <img src="https://img.icons8.com/?size=512&id=8uAtuJQJ4jhd&format=png" alt="" />
+            <span>{details?.vote_count}</span>
+            
+          </div>
         </div>
-      </div>
 
-      <div className="descript">
-        <h1>{details?.title}</h1>
-        <ul>
-          {details?.genres.map((genre) => (
-            <li key={genre.id}>{genre.name}</li>
-          ))}
-        </ul>
+        <div className="descript">
+          <h1>{details?.title}</h1>
+          <ul>
+            {details?.genres.map((genre) => (
+              <li key={genre.id}>{genre.name}</li>
+            ))}
+          </ul>
 
-        <p>{details?.overview}</p>
-        <p className="release_date">Lançamento: {details?.release_date.split('-').reverse().join('/')}</p>
-      </div>
+          <p>{details?.overview}</p>
+          <p className="release_date">Lançamento: {details?.release_date.split('-').reverse().join('/')}</p>
+        </div>
 
-      <div className="favorite_movie">
-        <p>Adicionar favorito</p>
-      </div>
+        <button className="favorite_movie" >Adicionar favorito</button>
+
+      </section>
     </StyledMovieDetails>
   )
 }
