9a2230092435fd36550efd9c19a9bbccf61e7b62
Flávio Eduardo <xflax00@gmail.com>
2023-07-17 22:53:55 -0300

    feat: add route search movies by name

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -2,6 +2,7 @@ import App from "../App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home.tsx";
 import MovieDetails from "../pages/MovieDetails/MovieDetails.tsx";
+import SearchMovie from "../pages/SearchMovie/SearchMovie.tsx";
 
 
 const Router = () => {
@@ -11,6 +12,7 @@ const Router = () => {
         <Route element={<App />} >
           <Route path="/" element={<Home />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/movies" element={<SearchMovie />} />
         </Route>
       </Routes>
     </BrowserRouter>
